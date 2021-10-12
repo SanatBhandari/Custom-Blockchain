65cf7d7a55d8c7deede92c83ab30c6c183512206
Sanat Bhandari <sanatbhandari33@gmail.com>
2021-10-12 18:15:29 -0500

    Added gitignore and setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Custom Blockchain',
+    version='1.0.0',
+    description='An example blockchain as a side project', 
+    author='Sanat Bhandari',
+    author_email='sanatbhandari33@gmail.com',
+    packages=find_packages()
+)
